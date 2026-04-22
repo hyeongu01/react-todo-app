7f6f505a1171a6ea2cf9bfed9f6d39a07ceb7eab
hyeongu01 <cdy5212@gmail.com>
2026-04-22 11:14:06 +0900

    feat: 테마 토글 버튼 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,12 +3,12 @@ import styles from './Header.module.css';
 import { useTheme } from '../contexts/ThemeContext';
 
 function Header(): JSX.Element {
-  const { toggleTheme } = useTheme();
+  const { theme, toggleTheme } = useTheme();
   return (
     <div className={styles.header}>
       <h1>📝 할 일 목록</h1>
 
-      <button onClick={toggleTheme}>테마</button>
+      <button onClick={toggleTheme}>{theme === 'light' ? '🌙' : '☀'}</button>
     </div>
   );
 }
